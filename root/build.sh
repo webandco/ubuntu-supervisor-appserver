2dd50a909987ca590948cfa3f94d8c45a21da024
David Spiola <david@webandco.com>
2016-05-01 22:01:19 +0200

    [task] cleanup build process

--- root/build.sh
+++ root/build.sh
@@ -1,13 +1,16 @@
 apt-get update
 
+#install git
+apt-get -y install git
+
+#setup composer
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+
 #install appserver
 echo "deb http://deb.appserver.io/ wheezy main" > /etc/apt/sources.list.d/appserver.list && wget http://deb.appserver.io/appserver.gpg -O - | apt-key add -
 apt-get -y update \
     && apt-get install -y appserver-dist \
     && rm -rf /var/lib/apt/lists/*
-
-#install git
-apt-get -y install git
 
 #delete package cache
 rm -rf /var/lib/apt/lists/*
@@ -15,8 +18,6 @@ rm -rf /var/lib/apt/lists/*
 #set php for commandline operations
 ln -s /opt/appserver/bin/php /usr/bin/php
 
-#setup composer
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
 #set helfpul flow commands
 cp /etc/.bash_aliases ~/.bash_aliases
