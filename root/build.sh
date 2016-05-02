db750bb90d156fa248d5ce2bd630a01efb8ffd3a
David Spiola <david@webandco.com>
2016-05-02 09:57:44 +0200

    [bug] fix compose setup

--- root/build.sh
+++ root/build.sh
@@ -3,9 +3,6 @@ apt-get update
 #install git
 apt-get -y install git
 
-#setup composer
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-
 #install appserver
 echo "deb http://deb.appserver.io/ wheezy main" > /etc/apt/sources.list.d/appserver.list && wget http://deb.appserver.io/appserver.gpg -O - | apt-key add -
 apt-get -y update \
@@ -17,6 +14,9 @@ rm -rf /var/lib/apt/lists/*
 
 #set php for commandline operations
 ln -s /opt/appserver/bin/php /usr/bin/php
+
+#setup composer
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
 
 
 #set helfpul flow commands
